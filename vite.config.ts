8d8182a6f1789b67c164d287c1220011c64f88c6
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-05-12 22:59:32 +0800

    chore: 将开发端口从1420更改为6086

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,14 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
+    port: 6086,
     strictPort: true,
     host: host || false,
     hmr: host
       ? {
           protocol: "ws",
           host,
-          port: 1421,
+          port: 6087,
         }
       : undefined,
     watch: {
